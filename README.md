b35222fdf42ec6aa2a6075a4f4cc20aa809891d5
Fabinzz <fabio200824@gmail.com>
2024-10-07 14:32:04 -0300

    feat: adição das categorias

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 # Instalação 
 1. Baixar e instalar o Node.js.
 2. Abrir o terminal e navegar até a pasta raiz do projeto.
-3. Instalar o MDB UI Kit com o seguinte comando:
-npm i mdb-ui-kit
+3. Instalar o MDB UI Kit com o seguinte comando: 
+npm install
--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const container = document.getElementById('card-container');
-    const cardHTML = `
+    const containerProducts = document.getElementById('card-container');
+    const containerCategories = document.getElementById('categories_container');
+    const cardProducts = `
         <div class="col-lg-4 col-md-12 mb-6 mb-lg-3">
             <div class="card">
                 <div class="bg-image hover-overlay" data-mdb-ripple-init data-mdb-ripple-color="light">
@@ -21,10 +22,22 @@ document.addEventListener('DOMContentLoaded', function() {
         </div>
     `;
 
-    // Número de colunas que você deseja replicar
-    const numberOfCards = 9;
+    const cardCategories = ` 
+                <div class="category"><a href="https://youtu.be/xvFZjo5PgG0">
+                    <img src="/images/anel.jpg" class="category-img-top" alt="Sunset Over the Sea"/>
+                    <div class="category-body">
+                      <p class="category-text">Anéis</p>
+                      <div class="category_line" align="center"></a></div>
+              `;
 
-    for (let i = 0; i < numberOfCards; i++) {
-        container.insertAdjacentHTML('beforeend', cardHTML);
+    // Número de colunas que você deseja replicar
+    const numberOfProducts = 9;
+    const numberOfCategories = 6;
+
+    for (let i = 0; i < numberOfProducts; i++) {
+        containerProducts.insertAdjacentHTML('beforeend', cardProducts);
+    }
+    for (let i = 0; i < numberOfCategories; i++) {
+        containerCategories.insertAdjacentHTML('beforeend', cardCategories);
     }
 });
